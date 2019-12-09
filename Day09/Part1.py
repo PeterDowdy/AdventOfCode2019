0083ef8f776f4e096263a3364cd06839fb76bbdd
Peter Dowdy <peter.dowdy@gmail.com>
2019-12-08 23:01:01 -0800

    Day 9

---
+++ Day09/Part1.py
@@ -0,0 +1,162 @@
+class pipe(object):
+    def __init__(self):
+        self.buf = []
+        self.last_output = 0
+    def next(self):
+        element = self.buf[0]
+        self.buf = self.buf[1:]
+        return element
+    def peek(self):
+        if len(self.buf) == 0:
+            return None
+        return self.buf[0]
+    def write(self,element):
+        self.last_output = element
+        self.buf.append(element)
+    def get_last_output(self):
+        return self.last_output
+
+class computer(object):
+    def __init__(self,inpipe, outpipe, debug = False):
+        self.debug = debug
+        self.total_counter = 0
+        self.relative_base = 0
+        self.finished = False
+        self.cur = 0
+        self.input_pipe = inpipe
+        self.output_pipe = outpipe
+        self.program = [1102,34463338,34463338,63,1007,63,34463338,63,1005,63,53,1102,1,3,1000,109,988,209,12,9,1000,209,6,209,3,203,0,1008,1000,1,63,1005,63,65,1008,1000,2,63,1005,63,902,1008,1000,0,63,1005,63,58,4,25,104,0,99,4,0,104,0,99,4,17,104,0,99,0,0,1102,1,37,1007,1102,24,1,1006,1102,26,1,1012,1101,528,0,1023,1102,256,1,1027,1102,466,1,1029,1102,1,629,1024,1101,0,620,1025,1101,0,0,1020,1102,1,30,1004,1101,39,0,1003,1102,36,1,1005,1102,531,1,1022,1102,32,1,1019,1101,0,27,1000,1101,0,28,1016,1101,1,0,1021,1101,23,0,1013,1102,1,25,1015,1102,1,21,1008,1102,1,22,1018,1102,1,34,1014,1102,475,1,1028,1101,33,0,1002,1101,0,35,1011,1102,1,20,1009,1102,38,1,1017,1101,259,0,1026,1101,31,0,1010,1101,0,29,1001,109,8,21102,40,1,10,1008,1018,40,63,1005,63,203,4,187,1105,1,207,1001,64,1,64,1002,64,2,64,109,7,21108,41,41,0,1005,1015,225,4,213,1106,0,229,1001,64,1,64,1002,64,2,64,109,1,1205,5,247,4,235,1001,64,1,64,1105,1,247,1002,64,2,64,109,20,2106,0,-9,1105,1,265,4,253,1001,64,1,64,1002,64,2,64,109,-38,1202,4,1,63,1008,63,33,63,1005,63,291,4,271,1001,64,1,64,1106,0,291,1002,64,2,64,109,6,2102,1,0,63,1008,63,29,63,1005,63,315,1001,64,1,64,1106,0,317,4,297,1002,64,2,64,109,10,21102,42,1,5,1008,1019,40,63,1005,63,341,1001,64,1,64,1105,1,343,4,323,1002,64,2,64,109,-13,2101,0,5,63,1008,63,24,63,1005,63,365,4,349,1105,1,369,1001,64,1,64,1002,64,2,64,109,7,1202,-6,1,63,1008,63,36,63,1005,63,389,1105,1,395,4,375,1001,64,1,64,1002,64,2,64,109,1,2107,31,-5,63,1005,63,411,1106,0,417,4,401,1001,64,1,64,1002,64,2,64,109,3,1206,8,431,4,423,1105,1,435,1001,64,1,64,1002,64,2,64,109,-8,2108,31,0,63,1005,63,451,1105,1,457,4,441,1001,64,1,64,1002,64,2,64,109,26,2106,0,-2,4,463,1001,64,1,64,1106,0,475,1002,64,2,64,109,-33,1207,6,38,63,1005,63,491,1106,0,497,4,481,1001,64,1,64,1002,64,2,64,109,3,2108,27,0,63,1005,63,515,4,503,1105,1,519,1001,64,1,64,1002,64,2,64,109,23,2105,1,0,1106,0,537,4,525,1001,64,1,64,1002,64,2,64,109,-30,1207,7,28,63,1005,63,559,4,543,1001,64,1,64,1106,0,559,1002,64,2,64,109,20,21101,43,0,0,1008,1013,43,63,1005,63,581,4,565,1105,1,585,1001,64,1,64,1002,64,2,64,109,-14,2102,1,1,63,1008,63,27,63,1005,63,611,4,591,1001,64,1,64,1105,1,611,1002,64,2,64,109,18,2105,1,7,4,617,1001,64,1,64,1106,0,629,1002,64,2,64,109,13,1206,-9,641,1105,1,647,4,635,1001,64,1,64,1002,64,2,64,109,-18,21107,44,45,-1,1005,1011,665,4,653,1105,1,669,1001,64,1,64,1002,64,2,64,109,-2,2107,28,-9,63,1005,63,687,4,675,1106,0,691,1001,64,1,64,1002,64,2,64,1205,10,701,1106,0,707,4,695,1001,64,1,64,1002,64,2,64,109,-6,1201,2,0,63,1008,63,21,63,1005,63,731,1001,64,1,64,1106,0,733,4,713,1002,64,2,64,109,-5,1208,7,23,63,1005,63,753,1001,64,1,64,1105,1,755,4,739,1002,64,2,64,109,16,1208,-8,37,63,1005,63,777,4,761,1001,64,1,64,1106,0,777,1002,64,2,64,109,3,21107,45,44,-8,1005,1010,797,1001,64,1,64,1105,1,799,4,783,1002,64,2,64,109,-8,1201,-5,0,63,1008,63,36,63,1005,63,821,4,805,1106,0,825,1001,64,1,64,1002,64,2,64,109,-9,2101,0,1,63,1008,63,31,63,1005,63,845,1105,1,851,4,831,1001,64,1,64,1002,64,2,64,109,6,21108,46,49,3,1005,1010,867,1106,0,873,4,857,1001,64,1,64,1002,64,2,64,109,5,21101,47,0,7,1008,1019,44,63,1005,63,897,1001,64,1,64,1106,0,899,4,879,4,64,99,21101,27,0,1,21102,913,1,0,1106,0,920,21201,1,30449,1,204,1,99,109,3,1207,-2,3,63,1005,63,962,21201,-2,-1,1,21101,940,0,0,1105,1,920,21202,1,1,-1,21201,-2,-3,1,21102,1,955,0,1106,0,920,22201,1,-1,-2,1105,1,966,22102,1,-2,-2,109,-3,2105,1,0]
+    
+    def write_positional(self,position,argument):
+        while position >= len(self.program):
+            self.program.append(0)
+        self.program[position] = argument
+    
+    def read_positional(self,position):
+        while position >= len(self.program):
+            self.program.append(0)
+        return self.program[position]
+    
+    def read_absolute(self,position):
+        return position
+    
+    def write_relative_base(self,position,argument):
+        while position+self.relative_base >= len(self.program):
+            self.program.append(0)
+        self.program[position+self.relative_base] = argument
+    
+    def read_relative_base(self,position):
+        while position+self.relative_base >= len(self.program):
+            self.program.append(0)
+        return self.program[position+self.relative_base]
+    
+    def get_accessors(self,mode):
+        if mode == 0: #positional mode
+            return (lambda a,b: self.write_positional(a,b),lambda a: self.read_positional(a))
+        if mode == 1: #absolute mode
+            return (lambda a,b: None,lambda a: self.read_absolute(a))
+        if mode == 2: #relative base mode
+            return (lambda a,b: self.write_relative_base(a,b), lambda a: self.read_relative_base(a))
+
+    def get_opcode(self,code):
+        strcode = str(code)
+        while len(strcode) < 5:
+            strcode = '0' + strcode
+        return (int(strcode[0]), int(strcode[1]), int(strcode[2]), int(strcode[3])*10+int(strcode[4]))
+    def is_finished(self):
+        return self.finished
+    def calculate(self):
+        self.total_counter = self.total_counter + 1
+        if self.program[self.cur] == 99:
+            if self.debug: print('Exit')
+            self.finished = True
+            return
+        while self.cur+3 >= len(self.program):
+            self.program.append(0)
+        (third_mode, second_mode, first_mode, opcode) = self.get_opcode(self.program[self.cur])
+        if self.debug: print(f'[{self.total_counter}|{self.cur}]: {self.program[self.cur]} = first:{"positional" if first_mode == 0 else "absolute" if first_mode == 1 else "relative base"}, second:{"positional" if second_mode == 0 else "absolute" if second_mode == 1 else "relative base"}, third:{"positional" if third_mode == 0 else "absolute" if third_mode == 1 else "relative base"} opcode:{opcode}')
+        first_accessors = self.get_accessors(first_mode)
+        (first_write,first_read) = first_accessors
+        second_accessors = self.get_accessors(second_mode)
+        (second_write,second_read) = second_accessors
+        third_accessors = self.get_accessors(third_mode)
+        third_write, third_read = third_accessors
+        first_node = self.program[self.cur+1]
+        second_node = self.program[self.cur+2]
+        third_node = self.program[self.cur+3]
+        if opcode == 1:
+            if self.debug: print('add')
+            if self.debug: print(f'{self.program[self.cur]},{self.program[self.cur+1]},{self.program[self.cur+2]},{self.program[self.cur+3]}')
+            third_write(third_node, first_read(first_node)+second_read(second_node))
+            if self.debug: print(f'{first_read(first_node)}+{second_read(second_node)}->program[{third_node}]')
+            self.cur = self.cur + 4
+        elif opcode == 2:
+            if self.debug: print('multiply')
+            if self.debug: print(f'{self.program[self.cur]},{self.program[self.cur+1]},{self.program[self.cur+2]},{self.program[self.cur+3]}')
+            third_write(third_node, first_read(first_node)*second_read(second_node))
+            if self.debug: print(f'{first_read(first_node)}*{second_read(second_node)}->program[{third_node}]')
+            self.cur = self.cur + 4
+        elif opcode == 3: #input
+            if self.debug: print('input')
+            if self.debug: print(f'{self.program[self.cur]},{self.program[self.cur+1]}')
+            if self.input_pipe.peek() is None:
+                return
+            next_input = self.input_pipe.next()
+            first_write(first_node,next_input)
+            if self.debug: print(f'input:{next_input}')
+            self.cur = self.cur + 2
+        elif opcode == 4: #output
+            if self.debug: print('output')
+            if self.debug: print(f'{self.program[self.cur]},{self.program[self.cur+1]}')
+            output_value = first_read(first_node)
+            self.output_pipe.write(output_value)
+            if self.debug: print(f'out: {output_value}')
+            self.cur = self.cur + 2
+        elif opcode == 5: #jump if not zero
+            if self.debug: print('jump if not zero')
+            if self.debug: print(f'{self.program[self.cur]},{self.program[self.cur+1]},{self.program[self.cur+2]}')
+            self.cur = second_read(second_node) if first_read(first_node) != 0 else self.cur + 3
+            if self.debug:
+                if first_read(first_node) != 0: print(f'{first_read(first_node)} != 0, cur = {second_read(second_node)}')
+                else: print(f'{first_read(first_node)} == 0, no jump')
+        elif opcode == 6: #jump if zero
+            if self.debug: print('jump if zero')
+            if self.debug: print(f'{self.program[self.cur]},{self.program[self.cur+1]},{self.program[self.cur+2]}')
+            self.cur = second_read(second_node) if first_read(first_node) == 0 else self.cur + 3
+            if self.debug:
+                if first_read(first_node) == 0: print(f'{first_read(first_node)} == 0, cur = {second_read(second_node)}')
+                else: print(f'{first_read(first_node)} != 0, no jump')
+        elif opcode == 7: #less than
+            if self.debug: print('less than')
+            if self.debug: print(f'{self.program[self.cur]},{self.program[self.cur+1]},{self.program[self.cur+2]},{self.program[self.cur+3]}')
+            third_write(third_node, 1 if first_read(first_node) < second_read(second_node) else 0)
+            if self.debug:
+                if first_read(first_node) < second_read(second_node): print(f'{first_read(first_node)} < {second_read(second_node)}, position {third_node} = 1')
+                else: print(f'{first_read(first_node)} >= {second_read(second_node)}, position {third_node} = 0')
+            self.cur = self.cur + 4
+        elif opcode == 8: #equal
+            if self.debug: print('equal')
+            if self.debug: print(f'{self.program[self.cur]},{self.program[self.cur+1]},{self.program[self.cur+2]},{self.program[self.cur+3]}')
+            third_write(third_node, 1 if first_read(first_node) == second_read(second_node) else 0)
+            if self.debug:
+                if first_read(first_node) == second_read(second_node): print(f'{first_read(first_node)} == {second_read(second_node)}, position {third_node} = 1')
+                else: print(f'{first_read(first_node)} != {second_read(second_node)}, position {third_node} = 0')
+            self.cur = self.cur + 4
+        elif opcode == 9:
+            if self.debug: print('alter relative base')
+            if self.debug: print(f'{self.program[self.cur]},{self.program[self.cur+1]}')
+            if self.debug:
+                print(f'relative base {self.relative_base} +/- {first_read(first_node)} = {self.relative_base+first_read(first_node)}')
+            self.relative_base = self.relative_base+first_read(first_node)
+            self.cur = self.cur + 2
+        else:
+            raise Exception(self.program[self.cur])
+
+inpipe = pipe()
+outpipe = pipe()
+computer = computer(inpipe,outpipe, False)
+inpipe.write(1)
+while not computer.finished:
+    computer.calculate()
+
+print(",".join([str(x) for x in outpipe.buf]))
